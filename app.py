a11299c0fa72aacc17ff63d13d57ef9faa379572
MaoJie <mcbchengdu@gmail.com>
2021-05-13 16:52:24 +0800

    v1

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 #coding: utf-8
 
 from flask import Flask
+from flask.templating import render_template
 app = Flask(__name__)
 
+
+
+
 @app.route('/')
-def hello():
-    return 'Welcome to index'
+def index():
+    return render_template('index.html',name=name,movies=movies)
 
 
